9bc044b272e2b8afb36f09869f49e01c62f25f21
Hozyain <mishapopovjke1377@gmail.com>
2025-12-28 14:54:33 +0300

    добавил main.py

---
+++ main.py
@@ -0,0 +1,36 @@
+import math
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+try:
+    link = "http://suninjuly.github.io/explicit_wait2.html"
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    def calc(x):
+        return str(math.log(abs(12 * math.sin(int(x)))))
+
+    wait = WebDriverWait(browser, 12)
+    element = wait.until(EC.text_to_be_present_in_element((By.ID, "price"),"$100"))
+    browser.find_element(By.ID, "book").click()
+
+    x_element = browser.find_element(By.ID, "input_value")
+    x = x_element.text
+    y = calc(x)
+    input1 = browser.find_element(By.ID, "answer")
+    input1.send_keys(y)
+
+
+    browser.find_element(By.ID, "solve").click()
+
+
+
+finally:
+    # ожидание чтобы визуально оценить результаты прохождения скрипта
+    time.sleep(5)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
